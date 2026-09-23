ef26a4fb6f12f5fb2a909ae0b0db00de7b9fd925
BernáthMilán(SZF_N_2 <BernthMiln(SZF_N_2@192.168.228.21>
2026-09-23 15:54:55 +0200

    kever metódus és switch case 1

--- src/main/Kever.java
+++ src/main/Kever.java
@@ -13,8 +13,33 @@ public class Kever {
     /**
      * @param args the command line arguments
      */
+    static String[] pakli = new String[22];
+    static String[] ujPakli = new String[22];
+    
     public static void main(String[] args) {
         // TODO code application logic here
+    }
+    
+    public static String[] kever(int tipp) {
+        switch (tipp) {
+            case 1:
+                for (int i = 1; i < 7; i++) {
+                    ujPakli[i]= pakli[20-((i-1)*3)];
+                    ujPakli[i]= pakli[19-((i-1)*3)];
+                    ujPakli[i]= pakli[21-((i-1)*3)];
+                }
+                break;
+            /*case:2*/
+                
+                
+                
+            /*case:3*/
+            
+               
+            default:
+                throw new AssertionError();
+        }
+        return ujPakli;
     }
     
 }
